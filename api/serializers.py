1273c4c76ccbe9ffb33746f4f78f547c54b9487e
NG-faye <ndeyegnilane00@gmail.com>
2026-01-30 16:06:46 +0100

    Final fix for username requirement

--- api/serializers.py
+++ api/serializers.py
@@ -3,26 +3,32 @@ from .models import Hotel
 from django.contrib.auth.models import User
 from djoser.serializers import UserCreateSerializer
 
-# 1. Serializer pour les Hôtels (Rétabli pour corriger l'ImportError)
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = '__all__'
 
-# 2. Serializer pour l'Utilisateur (Configuré pour éviter l'erreur de username)
 class UserSerializer(UserCreateSerializer):
-    username = serializers.CharField(required=False, allow_blank=True)
-
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'username', 'email', 'password', 'first_name')
+        extra_kwargs = {
+            'username': {'required': False, 'allow_blank': True},
+        }
 
     def create(self, validated_data):
-        # On récupère l'email pour l'utiliser comme nom d'utilisateur
+        # On extrait les données manuellement
         email = validated_data.get('email')
-        
-        # On force le username dans les données avant la création
-        validated_data['username'] = email
-        
-        # On utilise la méthode officielle de Django pour créer l'utilisateur
-        return User.objects.create_user(**validated_data)
+        password = validated_data.get('password')
+        first_name = validated_data.get('first_name', '')
+
+        # ON FORCE LA CRÉATION MANUELLE SANS PASSER PAR LE MANAGER CLASSIQUE
+        user = User.objects.create(
+            username=email, # Le username EST l'email
+            email=email,
+            first_name=first_name,
+            is_active=False # Important pour l'activation par mail
+        )
+        user.set_password(password) # On crypte le mot de passe
+        user.save()
+        return user
