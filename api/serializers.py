17eca07a1dd557a38b730976b0b680e47f9c4ff0
NG-faye <ndeyegnilane00@gmail.com>
2026-01-30 15:25:32 +0100

    Fix: Hard injection of username in create method

--- api/serializers.py
+++ api/serializers.py
@@ -9,15 +9,18 @@ class HotelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UserSerializer(UserCreateSerializer):
+    # On définit explicitement le champ username comme n'étant pas requis dans le JSON
+    # mais on va le remplir nous-mêmes.
+    username = serializers.CharField(required=False, allow_blank=True)
+
     class Meta(UserCreateSerializer.Meta):
         model = User
-        # On demande explicitement ces champs
-        fields = ('id', 'email', 'password', 'first_name')
+        fields = ('id', 'username', 'email', 'password', 'first_name')
 
     def create(self, validated_data):
-        # On récupère l'email
-        email = validated_data.get('email')
-        # On force le username à être l'email AVANT la création
-        validated_data['username'] = email
-        # On appelle la méthode de création de Djoser avec le username inclus
-        return super().create(validated_data)
+        # Si le frontend n'envoie pas de username, on prend l'email
+        if 'username' not in validated_data or not validated_data['username']:
+            validated_data['username'] = validated_data.get('email')
+        
+        # On utilise la méthode de création d'utilisateur de Django directement
+        return User.objects.create_user(**validated_data)
